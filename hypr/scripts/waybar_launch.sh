4308852d36c0bda30b2aca7e0b726b78db82cae8
Dominic <74207434+DominicDern@users.noreply.github.com>
2026-04-23 19:35:45 -0300

    Added /hypr/ back into repo, kinda important

---
+++ hypr/scripts/waybar_launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Terminate existing bars
+killall -q waybar
+
+# Wait until Hyprland is actually reporting monitors
+# This is more reliable than checking for the IPC socket variable
+while [ -z "$(hyprctl monitors -j | jq -r '.[] | .name')" ]; do
+  sleep 0.5
+done
+
+# Extra cushion for the compositor to finish internal setup
+sleep 1
+
+# Launch your bars
+waybar -c ~/.config/waybar/bar-one.jsonc &
+waybar -c ~/.config/waybar/bar-two.jsonc &
+
